2f73aef124320ac17afb84daa15409ea5cebd8f0
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-03-19 18:17:29 +0900

    ex23

---
+++ apg4b/ex23.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++){
+    cin >> A.at(i);
+  }
+
+  map<int, int> m;
+  for(int x : A){
+    if(m.count(x)){
+      m.at(x)++;
+    } else {
+      m[x] = 1;
+    }
+  }
+
+  int max_cnt = 0;
+  int ans = -1;
+
+  for(int x : A){
+    if (max_cnt < m.at(x)){
+      max_cnt = m.at(x);
+      ans = x;
+    } 
+  }
+
+  cout << ans << " " << max_cnt << endl;
+} 
