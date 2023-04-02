13f824921767d768d771cc36f53aa7a35b89267d
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-04-02 21:17:14 +0900

    a282

---
+++ A/a282.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int k;
+    cin >> k;
+
+    // string a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // for(int i = 1; i <= k; i++) cout << a[i-1];
+    // cout << endl;
+
+    for(int i = 0; i < k; i++) cout << char('A'+i);
+} 
