6a5f44c59001b6e0985a4aca3b21a01efc46347d
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-04-04 22:34:29 +0900

    b16

---
+++ tessoku/b16.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int h[100009], dp[100009];
+
+int main(){
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> h[i];
+
+    dp[1] = 0;
+    dp[2] = abs(h[2] - h[1]);
+    for(int i = 3; i <= N; i++){
+        dp[i] = min(dp[i-1]+abs(h[i]-h[i-1]), dp[i-2]+abs(h[i]-h[i-2]));
+    }
+
+    cout << dp[N] << endl;
+} 
