c389e1926138f3f3a90f0e788e80fca525e628db
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-04-10 22:43:31 +0900

    b277

---
+++ B/b277.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+	cin >> n;
+	vector<string> s(n);
+	for (int i = 0; i < n; i++) cin >> s[i];
+	bool ans = true;
+	for (int i = 0; i < n; i++) for (int j = 0; j < i; j++) if (s[i] == s[j]) ans = false;
+	string s1 = "HDCS";
+	string s2 = "A23456789TJQK";
+	for (int i = 0; i < n; i++) {
+		if (!count(s1.begin(), s1.end(), s[i][0]) || !count(s2.begin(), s2.end(), s[i][1])) ans = false;
+	}
+	cout << (ans ? "Yes" : "No") << endl;;
+} 
