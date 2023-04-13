bbed62fa27696ef5fe8281df33687bc01948b66e
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-04-13 09:53:52 +0900

    a31

---
+++ tessoku/a31.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long N;
+    cin >> N;
+    long long a = N/3;
+    long long b = N/5;
+    long long c = N/15;
+
+    cout << a + b - c  << endl;
+} 
