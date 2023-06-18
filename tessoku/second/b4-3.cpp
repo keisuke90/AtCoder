2bd295412587e3fc837346af87690b08f7b86dfb
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-06-18 11:55:46 +0900

    b4

---
+++ tessoku/second/b4-3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    string n;
+    cin >> n;
+
+    int ans = 0;
+    rep(i, 0, n.size()) ans += (n[i] - '0') * (1 << (n.size()-1-i));
+    cout << ans << endl;
+    return 0;
+} 
