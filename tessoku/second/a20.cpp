32b958f5739fb2b0c7b9b87087c470075ab9d58e
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-06-27 22:24:20 +0900

    a20修正

--- tessoku/second/a20.cpp
+++ tessoku/second/a20.cpp
@@ -8,18 +8,13 @@ int main(){
     cin >> s >> t;
     
     dp[0][0] = 0;
-    rep(i, 0, s.size()+1) {
-        rep(j, 0, t.size()+1) {
-            if(i >= 1 && j >= 1 && s[i-1] == t[j-1]) {
+    rep(i, 1, s.size()+1) {
+        rep(j, 1, t.size()+1) {
+            if(s[i-1] == t[j-1]) {
                 dp[i][j] = max({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+1});
-            } else if (i >= 1 && j>=1) {
+            } else {
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
-            // else if (i >= 1) {
-            //     dp[i][j] = dp[i-1][j];
-            // } else if (j >= 1) {
-            //     dp[i][j] = dp[i][j-1];
-            // }
         }
     }
 
