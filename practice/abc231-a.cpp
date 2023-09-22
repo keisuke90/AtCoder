ac95dba6347a2599555cc118f2941e60544a29dd
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-09-22 23:11:02 +0900

    abc231-a

---
+++ practice/abc231-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    int d;
+    cin >> d;
+    double ans = d / 100.0;
+    cout << ans << endl;
+
+    return 0;
+} 
