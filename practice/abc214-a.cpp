c9f13144014d9a263c3cd0bb254eb68400cb9890
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-10-03 22:21:42 +0900

    abc214-a

---
+++ practice/abc214-a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n < 126) {
+        cout << 4 << endl;
+    } else if(n < 212) {
+        cout << 6 << endl;
+    } else {
+        cout << 8 << endl;
+    }
+
+    return 0;
+} 
