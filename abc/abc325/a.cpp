fc0860e6268eec03b8338d2cbd3a2b9df3250df1
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-10-21 22:54:01 +0900

    a

---
+++ abc/abc325/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    string s, t;
+    cin >> s >> t;
+    cout << s << ' ' << "san" << endl;
+
+    return 0;
+} 
