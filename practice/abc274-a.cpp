128b2e6a35afcab84be3ebced19cb6c3bf8b0d38
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-11-03 18:13:56 +0900

    abc274-a

---
+++ practice/abc274-a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    double a, b;
+    cin >> a >> b;
+    cout << fixed << setprecision(3)  << b/a << endl;
+    return 0;
+} 
