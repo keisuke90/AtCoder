265f213bd43af6470e57970d1a74f1a77b1bdafa
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-11-03 18:13:21 +0900

    abc001-a

---
+++ practice/abc001-a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    int h1, h2;
+    cin >> h1 >> h2;
+    cout << h1 - h2 << endl;
+
+    return 0;
+} 
