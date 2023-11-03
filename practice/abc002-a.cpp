9ecc98fce798b7bd8c16678101ddda8a2ba159f0
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-11-03 18:13:27 +0900

    abc002-a

---
+++ practice/abc002-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+
+    cout << max(x, y) << endl;
+
+    return 0;
+} 
