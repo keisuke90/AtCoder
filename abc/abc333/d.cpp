de6ca93ea287f41bee0d10244cfd6ed1663407b0
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-12-17 15:52:16 +0900

    精進

---
+++ abc/abc333/d.cpp
@@ -0,0 +1,96 @@
+// #include <bits/stdc++.h>
+// #define rep(i, start, end) for(int i = start; i < end; i++)
+// using namespace std;
+
+// struct dsu {
+//   public:
+//     dsu() : _n(0) {}
+//     explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+//     int merge(int a, int b) {
+//         assert(0 <= a && a < _n);
+//         assert(0 <= b && b < _n);
+//         int x = leader(a), y = leader(b);
+//         if (x == y) return x;
+//         if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+//         parent_or_size[x] += parent_or_size[y];
+//         parent_or_size[y] = x;
+//         return x;
+//     }
+
+//     bool same(int a, int b) {
+//         assert(0 <= a && a < _n);
+//         assert(0 <= b && b < _n);
+//         return leader(a) == leader(b);
+//     }
+
+//     int leader(int a) {
+//         assert(0 <= a && a < _n);
+//         if (parent_or_size[a] < 0) return a;
+//         return parent_or_size[a] = leader(parent_or_size[a]);
+//     }
+
+//     int size(int a) {
+//         assert(0 <= a && a < _n);
+//         return -parent_or_size[leader(a)];
+//     }
+
+//     std::vector<std::vector<int>> groups() {
+//         std::vector<int> leader_buf(_n), group_size(_n);
+//         for (int i = 0; i < _n; i++) {
+//             leader_buf[i] = leader(i);
+//             group_size[leader_buf[i]]++;
+//         }
+//         std::vector<std::vector<int>> result(_n);
+//         for (int i = 0; i < _n; i++) {
+//             result[i].reserve(group_size[i]);
+//         }
+//         for (int i = 0; i < _n; i++) {
+//             result[leader_buf[i]].push_back(i);
+//         }
+//         result.erase(
+//             std::remove_if(result.begin(), result.end(),
+//                            [&](const std::vector<int>& v) { return v.empty(); }),
+//             result.end());
+//         return result;
+//     }
+
+//   private:
+//     int _n;
+//     // root node: -1 * component size
+//     // otherwise: parent
+//     std::vector<int> parent_or_size;
+// };
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     dsu uf(n);
+
+//     rep(i, 0, n-1) {
+//         int a, b;
+//         cin >> a >> b;
+//         a--;
+//         b--;
+//         if(a!=0 && b!=0) {
+//             uf.merge(a, b);
+//         }
+//     }
+
+//     int tmp = 0;
+//     rep(i, 0, n-1) {
+//         tmp = max(tmp, uf.size(i));
+//     }
+//     cout << n - tmp << endl;
+
+//     return 0;
+// } 
+
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+
+    return 0;
+} 
