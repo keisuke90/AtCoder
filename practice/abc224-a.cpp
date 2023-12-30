1cf9af10a8ea3fe13585c2341a51a9dc92b78035
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-12-30 23:21:43 +0900

    精進

---
+++ practice/abc224-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    if(s[s.size()-1] == 'r') cout << "er" << endl;
+    else cout << "ist" << endl;
+
+    return 0;
+} 
