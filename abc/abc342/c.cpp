a26fb1deec2d07d8b0d5de1cf9a9d7331a1fd8d4
KeisukeKuwahara <kuwa.90k@gmail.com>
2024-02-25 11:59:15 +0900

    精進

--- abc/abc342/c.cpp
+++ abc/abc342/c.cpp
@@ -1,64 +1,29 @@
-// #include <bits/stdc++.h>
-// #define rep(i, start, end) for(int i = start; i < end; i++)
-// using namespace std;
-
-// int main(){
-//     int n, q;
-//     string s;
-//     cin >> n >> s >> q;
-
-//     map<char, char> mp1;
-//     map<char, char> mp2;
-
-//     rep(i, 0, q) {
-//         char c, d;
-//         cin >> c >> d;
-//         if(mp2.count(c) == 0) {
-//             mp1[c] = d;
-//             mp2[d] = c;
-//         } else {
-//             mp1[c] = d;
-//             mp1[mp2[c]] = d;
-//             mp2[c] = d;
-//         }
-//     }
-//     rep(i, 0, n) {
-//         if(mp1[s[i]] == 0) cout << s[i];
-//         else cout << mp1[s[i]];
-//     }
-//     cout << endl;
-
-//     return 0;
-// } 
-
 #include <bits/stdc++.h>
+#define rep(i, start, end) for(int i = start; i < end; i++)
 using namespace std;
 
-int main() {
+int main(){
     int n, q;
     string s;
     cin >> n >> s >> q;
-
-    // 英小文字のための変換マップ
-    vector<char> transform(26);
-    for (char c = 'a'; c <= 'z'; ++c) {
-        transform[c - 'a'] = c;
+    map<char, char> mp;
+    string abc = "abcdefghijklmnopqrstuvwxyz";
+    rep(i, 0, abc.size()) {
+        mp[abc[i]] = abc[i];
     }
 
-    for (int i = 0; i < q; ++i) {
+    rep(i, 0, q) {
         char c, d;
         cin >> c >> d;
-        for (int j = 0; j < 26; ++j) {
-            if (transform[j] == c) {
-                transform[j] = d;
-            }
+        for(auto v : mp) {
+            if(v.second == c) mp[v.first] = d;
         }
     }
 
-    for (char &c : s) {
-        c = transform[c - 'a'];
+    rep(i, 0, s.size()) {
+        cout << mp[s[i]];
     }
-    cout << s << endl;
+    cout << endl;
 
     return 0;
-}
+} 
