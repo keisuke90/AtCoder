e02eebb812e118ffa2b9be5bfb9f05bb762489eb
KeisukeKuwahara <kuwa.90k@gmail.com>
2024-05-05 00:03:30 +0900

    別解

--- abc/abc352/c.cpp
+++ abc/abc352/c.cpp
@@ -1,3 +1,31 @@
+// #include <bits/stdc++.h>
+// #define rep(i, start, end) for(int i = start; i < end; i++)
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> a(n), b(n);
+//     rep(i, 0, n) {
+//         cin >> a[i] >> b[i];
+//     }
+
+//     long long ans = 0;
+//     int p = -1;
+//     int min_n = 0;
+//     rep(i, 0, n) {
+//         if(abs(a[i]-b[i]) > min_n) {
+//             p = i;
+//             min_n = abs(a[i] - b[i]);
+//         }
+//         ans += a[i];
+//     }
+//     ans+=b[p];
+//     ans-=a[p];
+//     cout << ans << endl;
+//     return 0;
+// } 
+
 #include <bits/stdc++.h>
 #define rep(i, start, end) for(int i = start; i < end; i++)
 using namespace std;
@@ -5,23 +33,17 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<int> a(n), b(n);
+    vector<long long> a(n), b(n);
+    long long cnt = 0;
     rep(i, 0, n) {
         cin >> a[i] >> b[i];
+        cnt += a[i];
     }
-
     long long ans = 0;
-    int p = -1;
-    int min_n = 0;
     rep(i, 0, n) {
-        if(abs(a[i]-b[i]) > min_n) {
-            p = i;
-            min_n = abs(a[i] - b[i]);
-        }
-        ans += a[i];
+        ans = max(ans, cnt + b[i] - a[i]);
     }
-    ans+=b[p];
-    ans-=a[p];
     cout << ans << endl;
+
     return 0;
 } 
--- abc/abc352/d.cpp
+++ abc/abc352/d.cpp
@@ -1,3 +1,45 @@
+// #include <bits/stdc++.h>
+// #define rep(i, start, end) for(int i = start; i < end; i++)
+// using namespace std;
+
+// int main(){
+//     int n, k;
+//     cin >> n >> k;
+//     vector<int> p(n), pos(n+1);
+//     rep(i, 0, n) {
+//         cin >> p[i];
+//         pos[p[i]] = i+1;
+//     }
+
+//     queue<int> q;
+//     priority_queue<int> pq1;
+//     priority_queue<int, vector<int>, greater<int>> pq2;
+//     vector<bool> used(n+1);
+//     int ans = 1e9;
+//     rep(i, 1, n+1) {
+//         if(q.size() < k) {
+//             q.push(pos[i]);
+//             pq1.push(pos[i]);
+//             pq2.push(pos[i]);
+//         } else {
+//             while(used[pq1.top()]) pq1.pop();
+//             while(used[pq2.top()]) pq2.pop();
+//             ans = min(ans, pq1.top()-pq2.top());
+//             int now = q.front(); q.pop();
+//             used[now] = true;
+//             if(pq1.top() == now) pq1.pop();
+//             if(pq2.top() == now) pq2.pop();
+//             q.push(pos[i]);
+//             pq1.push(pos[i]);
+//             pq2.push(pos[i]);
+//         }
+//     }
+//     ans = min(ans, pq1.top()-pq2.top());
+//     cout << ans << endl;
+
+//     return 0;
+// } 
+
 #include <bits/stdc++.h>
 #define rep(i, start, end) for(int i = start; i < end; i++)
 using namespace std;
@@ -5,37 +47,21 @@ using namespace std;
 int main(){
     int n, k;
     cin >> n >> k;
-    vector<int> p(n), pos(n+1);
+    vector<int> pos(n);
     rep(i, 0, n) {
-        cin >> p[i];
-        pos[p[i]] = i+1;
+        int p;
+        cin >> p;
+        pos[p-1] = i;
     }
 
-    queue<int> q;
-    priority_queue<int> pq1;
-    priority_queue<int, vector<int>, greater<int>> pq2;
-    vector<bool> used(n+1);
-    int ans = 1e9;
-    rep(i, 1, n+1) {
-        if(q.size() < k) {
-            q.push(pos[i]);
-            pq1.push(pos[i]);
-            pq2.push(pos[i]);
-        } else {
-            while(used[pq1.top()]) pq1.pop();
-            while(used[pq2.top()]) pq2.pop();
-            ans = min(ans, pq1.top()-pq2.top());
-            int now = q.front(); q.pop();
-            used[now] = true;
-            if(pq1.top() == now) pq1.pop();
-            if(pq2.top() == now) pq2.pop();
-            q.push(pos[i]);
-            pq1.push(pos[i]);
-            pq2.push(pos[i]);
-        }
+    set<long long> st;
+    rep(i, 0, k) st.insert(pos[i]);
+    long long ans = *st.rbegin() - *st.begin();
+    rep(i, k, n) {
+        st.erase(pos[i-k]);
+        st.insert(pos[i]);
+        ans = min(ans, *st.rbegin() - *st.begin());
     }
-    ans = min(ans, pq1.top()-pq2.top());
     cout << ans << endl;
-
     return 0;
 } 
